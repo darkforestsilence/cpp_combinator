2f4cdcc4570d02b3a1bed25362eb406d8038bf59
Amy Larane <amy@pop-os.fios-router.home>
2020-11-06 16:19:01 -0600

    Fixed some changes
    Starting Over

--- src/main.cpp
+++ src/main.cpp
@@ -2,42 +2,7 @@
 #include<string>
 #include<list>
 
-#include"parser.h"
-
 using namespace std;
-
-ParserReturn<unsigned int, char> integer(ParserInput<char> input){
-	auto number = some(matchPred<char, char>([](char c){
-		return c >= '0' && c <= '9';
-	}));
-
-	auto [result, rest] = number(input);
-
-	return make_pair(opfold<char, int>(
-		[](char c, int i) -> int { return (i * 10) + (c- '0'); },
-		result,
-		0), rest);
-}
-
 int main(void){
-	
-
-	// get some input to check
-	cout << "Input test string: ";
-	string input;
-	getline(cin, input);
-
-	// run the parser
-	auto [i, rest] = integer(
-		list(input.begin(), input.end()));
-
-	if(i){
-		cout << "Parsed Successfully: " << *i << endl;
-	} else {
-		// we didn't have a result
-		cout << "Parse Error" << endl;
-
-	}
-
 	return 0;
 }
